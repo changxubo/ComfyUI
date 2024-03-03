2916d44b0cdfe5d6e3b04f44fb63148e89656987
Bob Chang <bob.chang@amway.com>
2024-03-03 18:09:02 +0800

    add folder

--- custom_nodes/__init__.py
+++
@@ -1,5 +0,0 @@
-from .train import LoraTraininginComfy, LoraTraininginComfyAdvanced, TensorboardAccess
-from .image_resize import ImageResize
-NODE_CLASS_MAPPINGS = {"Lora Training in ComfyUI": LoraTraininginComfy, "Lora Training in Comfy (Advanced)": LoraTraininginComfyAdvanced, "Tensorboard Access": TensorboardAccess, "ImageResize": ImageResize}
-NODE_DISPLAY_NAME_MAPPINGS = {  "ImageResize": "Image Resize"}
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
